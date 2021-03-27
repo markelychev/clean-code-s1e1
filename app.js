1fbba65f7516ff38292572bbd5a6f22c13b7f924
A.Markelychev <a.markelychev@gmail.com>
2021-03-27 14:59:30 +0300

    refactor: change class names according to rule 3.2 in html-and-css

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ var createNewTaskElement=function(taskString){
     editInput.className="task-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn-edit";
     editButton.classList.add('btn');
 
-    deleteButton.className="delete";
+    deleteButton.className="btn-delete";
     deleteButton.classList.add('btn');
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="btn-bg"
@@ -89,7 +89,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".btn-edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -161,8 +161,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.btn-edit");
+    var deleteButton=taskListItem.querySelector("button.btn-delete");
 
 
     //Bind editTask to edit button.
